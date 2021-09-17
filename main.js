8f15473abe434cd88b3812da99518b5c34aae3f3
dongnycklee <89719426+dongnycklee@users.noreply.github.com>
2021-09-18 01:26:34 +0900

    js perpect

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ navbarMenu.addEventListener('click', (event) => {
   //스몰 메뉴바 클릭시 메뉴바 remove 
   navbarMenu.classList.remove('open');
   scrollIntoView(link);
-  
 });
 
 // 네비게이션바 휴대폰사이즈 스몰메뉴 활성화
@@ -104,8 +103,72 @@ workBtnContainer.addEventListener('click', (e) => {
   }, 300);
 });
 
-/* 스크롤 함수 */
+
+/* 스크롤시 observer를 활용해 네비게이션 메뉴바 border 활성화 */
+
+//sectionids 배열에 메뉴에있는 모든 아이디를 문자열로 배열로저장
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',
+];
+// 모든 sectionsids 요소들을 map 을 활용해 sections 배열에 할당
+const sections = sectionIds.map(id => document.querySelector(id));
+// 동일한 네비게이션 아이템 요소들을 navItems에 할당
+const navItems = sectionIds.map(id =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+//현재 선택된 메뉴 index와 메뉴 요소를 변수에 저장
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+// 새로운 메뉴를 선택할때마다 이전에 활성화된 요소를 지워주고 새롭게 할당한후 active 지정
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+/* 스크롤 smooth 이동 */
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+// observer를 이용해서 section이 화면밖으로 나갈때마다 그다음에 해당하는 인덱스를 할당
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+// 위 할당한 인덱스값을 활용해서 사용자가 스크롤을 쓸때마다 스크롤위치가 제일위라면 
+// 제일위에있는 인덱스값을 설정하고 제일아래라면 제일마지막에있는 메뉴아이템을 선택
+// 중간에있는거라면 위 observer 를 활용하여 계산후 할당된 내부 인덱스를 활용해서 메뉴아이템을 선택
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+  if(window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight) {
+    selectedNavIndex = navItems.length -1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+})
